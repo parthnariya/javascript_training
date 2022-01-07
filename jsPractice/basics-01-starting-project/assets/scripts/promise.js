1734f2485fa8f756ba5684f19b11d792f6eb9748
parth <parth@mail.com>
2022-01-07 17:49:08 +0530

    path bug resolve

--- jsPractice/basics-01-starting-project/assets/scripts/promise.js
+++ jsPractice/basics-01-starting-project/assets/scripts/promise.js
@@ -3,11 +3,34 @@ const url = [
   "https://jsonplaceholder.typicode.com/posts",
 ];
 
-Promise.all(
-  url.map(link => {
-    return fetch(link).then((res) => res.json());
-  })
-).then((results) => {
-  console.log(results[0]);
-  console.log(results[1]);
-});
+// Promise.all(
+//   url.map(link => {
+//     return fetch(link).then((res) => res.json());
+//   })
+// ).then((results) => {
+//   console.log(results[0]);
+//   console.log(results[1]);
+// });
+
+// const getData = async function () {
+//   try {
+//     const [user, posts] = await Promise.all(
+//       url.map((link) => {
+//         return fetch(link).then((res) => res.json());
+//       })
+//     );
+//     console.log(user);
+//     console.log(posts);
+//   } catch(err) {
+//     console.log(err);
+//   }
+// };
+// getData();
+
+const getData2 = async function () {
+    const arrayOfLinks = url.map(link => fetch(link));
+    for await ( let request of arrayOfLinks){
+        const data = await request.json();
+        console.log(data);
+    }
+}
